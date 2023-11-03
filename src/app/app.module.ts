95fcbdaf0c0e596c9e40d07dedba7dcd1b9763fb
Gerardo-Isc <gerardomtzisc@gmail.com>
2023-11-03 12:23:24 -0600

    feat(ui): :lipstick: Add navbar and its animation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,25 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HomeComponent } from './web/home/home.component';
+import { ProjectsComponent } from './web/projects/projects.component';
+import { ExtraOptions, RouterModule, Routes } from '@angular/router';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    NavbarComponent,
+    HomeComponent,
+    ProjectsComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
